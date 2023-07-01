1d7b493d697e8bd17b5d7aec658d7543f130c076
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-01 23:28:46 +0500

    Updated

---
+++ Day1_Data_Types.py
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = "HackerRank "
+
+# Declare second integer, double, and String variables.
+number = 0
+double_value = 0.0
+string_value = "of Asad Ali"
+
+# Read and save an integer, double, and String to your variables.
+number = int(input())
+double_value = float(input())
+string_value = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + number)
+
+# Print the sum of the double variables on a new line.
+print(d + double_value)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + string_value)
