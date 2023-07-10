82676a473487d66a2d1a7ccc71dc1fb311068dbc
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-10 22:58:31 +0500

    Updated

--- Day2 - Operators.py
+++ Day2 - Operators.py
@@ -1,20 +1,3 @@
-#!/bin/python3
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'solve' function below.
-#
-# The function accepts following parameters:
-#  1. DOUBLE meal_cost
-#  2. INTEGER tip_percent
-#  3. INTEGER tax_percent
-#
-
-
 def solve(meal_cost, tip_percent, tax_percent):
     print(
         round(
